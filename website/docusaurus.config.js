6e12ff0d8834719c6175d777f2aed19214b0a3fc
Cristian Moldovan <crissmoldovan@gmail.com>
2020-08-31 04:44:14 +0300

    a bit of minimal styling

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,7 +11,7 @@ module.exports = {
     navbar: {
       logo: {
         alt: "React MX logo",
-        src: "https://i.imgur.com/n7UZNjk.png",
+        src: "/img/mx-logo-transparent.svg",
       },
       items: [
         {
@@ -65,5 +65,10 @@ module.exports = {
         },
       },
     ],
+  ],
+  stylesheets: [
+    "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap",
+    "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
+    "https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,423;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,423;1,500;1,600;1,700&display=swap",
   ],
 };
